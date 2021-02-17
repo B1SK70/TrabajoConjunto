e1d946a7e9ca8aa3bb5ceef47e87717a46f08e10
Jesus Cordoba <jesuscordoba2009@gmail.com>
2021-02-17 19:46:52 +0100

    Pagina Home

---
+++ Laravel/resources/views/index.blade.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+    <title>Inicio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body class="bg-primary">
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/tipos">Tipos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <h1 class="text-center text-light" style="margin-top: 100px;">Inicio</h1>
+    <div class="container">
+        <div class="bg-light text-center abs-center mt-3 p-3 rounded">
+            <h1>Patata</h1>
+            <hr>
+            <p>Solanum tuberosum, de nombre común papa1​ o patata,2​ es una especie herbácea perteneciente al género
+                Solanum de la familia de las solanáceas, originaria de la región que comprende el altiplano sur del
+                Perú3​ y el noroccidente de Bolivia.4​ Fue domesticada en el altiplano andino y en las cercanías del
+                lago Titicaca por los habitantes de esta región desde hace unos 8000 años.5​6​ En el siglo XVI comenzó a
+                ser trasladada a Europa por los conquistadores españoles quienes la consideraban una curiosidad botánica
+                y no una planta alimenticia. Su consumo fue creciendo, aunque al principio como planta forrajera y de
+                jardín por sus flores; su uso gastronómico se expandió a todo el mundo desde el siglo XVIII gracias a
+                los escritos agronómicos del francés Antoine Parmentier y del irlandés afincado en España Enrique Doyle,
+                hasta convertirse en uno de los principales alimentos del ser humano.
+            </p>
+            <hr>
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
