4af386ff312d30049e2133aa190efb4d3d70391c
Ismael <igonzalezbravo@iessonferrer.net>
2021-02-17 20:11:05 +0100

    Crear archivo tipos de patata

---
+++ Laravel/resources/views/tipos.blade.php
@@ -0,0 +1,27 @@
+@include('head')
+
+@include('header')
+
+    <h1 class="text-center text-light" style="margin-top: 100px;">Inicio</h1>
+    <div class="container">
+        <div class="bg-light text-center abs-center mt-3 p-3 rounded">
+            <h1>Tipos de patatas</h1>
+            <hr>
+            <h3>Monalisa</h3>
+            <p>La patata Monalisa es una de las variedades preferidas por el público porque se mantiene entera durante la cocción, es económica y absorbe poco aceite, siendo de forma regular. Esta patata es ideal para hornear o guisar, pero también responde bien si la vas a freír. Por este motivo, te la encontrarás en la mayoría de fruterías del país
+            </p>
+            <hr>
+            <h3>Buffet</h3>
+            <p>Esta patata con carne cremosa y blanca destaca por ser muy sabrosa y resulta deliciosa frita.
+            </p>
+            <hr>
+            <h3>Red Pontiac</h3>
+            <p>La variedad Red Pontiac, también denominada roja, puede estar varios días en la nevera sin problemas y es idónea para hacer guisos. Este tipo de patata es roja por fuera y blanca por dentro.
+            </p>
+            <hr>
+
+        </div>
+    </div>
+
+@include('footer')
+
