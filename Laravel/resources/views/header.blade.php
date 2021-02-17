6f9f983ca0e0e2f08fbae914f8753ff0c38e045f
Jesus Cordoba <jesuscordoba2009@gmail.com>
2021-02-17 20:02:46 +0100

    Primeros cambios plantilla blade

---
+++ Laravel/resources/views/header.blade.php
@@ -0,0 +1,20 @@
+<body class="bg-primary">
+    
+    <header>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/tipos">Tipos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
