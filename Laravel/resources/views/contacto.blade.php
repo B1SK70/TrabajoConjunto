6ab12c2ebb65fdb78f9efd496d467d2f8d4c3ff9
David Belzunces Candel <davidbelzunces6@gmail.com>
2021-02-19 23:44:18 +0100

    Config Mail 2

--- Laravel/resources/views/contacto.blade.php
+++ Laravel/resources/views/contacto.blade.php
@@ -1,5 +1,4 @@
 @include('head')
-<<<<<<< HEAD
 @include('header')
 
   <main style="margin-top: 100px;">
@@ -33,31 +32,3 @@
 </body>
 
   @include('footer')
-=======
-
-@include('header')
-
-<h1 class="text-center text-light" style="margin-top: 100px;">Contáctanos</h1>
-<div class="container">
-    <div class="bg-light text-center abs-center mt-3 p-3 rounded">
-        <hr>
-        <form>
-            <div class="form-group">
-              <label for="exampleInputFullName">Nombre completo</label>
-              <input type="text" class="form-control" id="exampleInputFullName" placeholder="Introduce tu nombre...">
-            </div>
-            <div class="form-group">
-              <label for="exampleInputSubject">Asunto</label>
-              <input type="text" class="form-control" id="exampleInputSubject" placeholder="Asunto...">
-            </div>
-            <div class="form-group">
-                <label for="exampleTextArea">Mensaje</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Escribe aquí..."></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
-          </form>
-    </div>
-</div>
-
-@include('footer')
->>>>>>> David
